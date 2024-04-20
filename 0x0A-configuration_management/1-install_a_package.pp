6b2b4aaa2369d9b0aad6122f63683fd9514fb0dc
gboigwe <geakande@gmail.com>
2024-04-20 15:06:11 +0100

    Using Puppet install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 #Using Puppet, install flask from pip3
-class { 'python' }
 
-package { 'python-devel':
-    ensure => installed,
-}
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Class['python::devel'],
 }
