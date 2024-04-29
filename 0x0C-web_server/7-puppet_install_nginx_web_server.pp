4f2ccc9ee6cdd246ba490b38780b23bb76f9a513
gboigwe <geakande@gmail.com>
2024-04-29 11:42:18 +0100

    Using puppet to configure server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Install Nginx web server Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/gboigwe permanent;',
+}
+
+file { '/var/www/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx']',
+}
