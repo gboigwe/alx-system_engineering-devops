f83687b49fc83dd45a5142a99cd677ceaf258951
gboigwe <geakande@gmail.com>
2024-05-21 17:52:46 +0100

    Script for using REST Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
 
-    response = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", param={"userId": sys.argv[1]}).json()
 
     done_tasks = [task for task in todos if task["completed"]]
