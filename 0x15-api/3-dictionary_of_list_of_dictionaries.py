57da6ada9836442ea3a2a0d45ce8fdc32f508c25
gboigwe <geakande@gmail.com>
2024-06-02 16:49:51 +0100

    Updated the get process api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,43 +9,57 @@ import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-    # Get all users
-    users_response = requests.get(url + "users")
-    if users_response.status_code == 200:
-        users = users_response.json()
-    else:
-        print("Failed to get users")
-        exit(1)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
 
-    # Get all todos
-    todos_response = requests.get(url + "todos")
-    if todos_response.status_code == 200:
-        todos = todos_response.json()
-    else:
-        print("Failed to get todos")
-        exit(1)
+# if __name__ == "__main__":
+#     url = "https://jsonplaceholder.typicode.com/"
 
-    # Group todos by user
-    todo_lists = {}
-    for todo in todos:
-        user_id = todo["userId"]
-        username = next(
-            (user["name"] for user in users if user["id"] == user_id), "Unknown"
-        )
-        task_data = {
-            "username": username,
-            "task": todo["title"],
-            "completed": todo["completed"],
-        }
-        if user_id in todo_lists:
-            todo_lists[user_id].append(task_data)
-        else:
-            todo_lists[user_id] = [task_data]
+#     # Get all users
+#     users_response = requests.get(url + "users")
+#     if users_response.status_code == 200:
+#         users = users_response.json()
+#     else:
+#         print("Failed to get users")
+#         exit(1)
 
-    # Export todo lists to JSON
-    file_name = "todo_all_employees.json"
-    with open(file_name, "w") as jsonfile:
-        json.dump(todo_lists, jsonfile)
+#     # Get all todos
+#     todos_response = requests.get(url + "todos")
+#     if todos_response.status_code == 200:
+#         todos = todos_response.json()
+#     else:
+#         print("Failed to get todos")
+#         exit(1)
 
-    print(f"Todo lists of all employees exported to {file_name}")
+#     # Group todos by user
+#     todo_lists = {}
+#     for todo in todos:
+#         user_id = todo["userId"]
+#         username = next(
+#             (user["name"] for user in users if user["id"] == user_id), "Unknown"
+#         )
+#         task_data = {
+#             "username": username,
+#             "task": todo["title"],
+#             "completed": todo["completed"],
+#         }
+#         if user_id in todo_lists:
+#             todo_lists[user_id].append(task_data)
+#         else:
+#             todo_lists[user_id] = [task_data]
+
+#     # Export todo lists to JSON
+#     file_name = "todo_all_employees.json"
+#     with open(file_name, "w") as jsonfile:
+#         json.dump(todo_lists, jsonfile)
+
+#     print(f"Todo lists of all employees exported to {file_name}")
