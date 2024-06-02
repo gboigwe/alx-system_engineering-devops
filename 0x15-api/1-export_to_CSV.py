4d30f4282ce2c2a3ccd476ab05bf8b488a425989
gboigwe <geakande@gmail.com>
2024-06-02 16:19:31 +0100

    Updated the get process api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,44 +11,51 @@ import sys
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) != 2:
-    #     print("Usage: ./1-export_to_CSV.py <employee_id>")
-    #     exit(1)
-
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Get employee name
-    response = requests.get(url + "users/{}".format(employee_id))
-    if response.status_code == 200:
-        user = response.json()
-    else:
-        print("Failed to get employee name for ID {}".format(employee_id))
-        exit(1)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
 
-    # Get employee's todo list
-    todos_response = requests.get(url + "todos", params={
-        "userId": employee_id
-        })
-    if todos_response.status_code == 200:
-        todos = todos_response.json()
-    else:
-        print("Failed to get todo list for employee {}"
-              .format(user.get('name')))
-        exit(1)
+# if __name__ == "__main__":
+#     employee_id = sys.argv[1]
+#     url = "https://jsonplaceholder.typicode.com/"
+#     response = requests.get(url + "users/{}".format(employee_id))
+#     if response.status_code == 200:
+#         user = response.json()
+#     else:
+#         print("Failed to get employee name for ID {}".format(employee_id))
+#         exit(1)
 
-    # Export todo list to CSV
-    file_name = "{}.csv".format(employee_id)
-    with open(file_name, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            task_status = "True" if task["completed"] else "False"
-            csv_writer.writerow([
-                employee_id,
-                user.get("name"),
-                task_status,
-                task["title"]
-            ])
+#     # Get employee's todo list
+#     todos_response = requests.get(url + "todos", params={
+#         "userId": employee_id
+#         })
+#     if todos_response.status_code == 200:
+#         todos = todos_response.json()
+#     else:
+#         print("Failed to get todo list for employee {}"
+#               .format(user.get('name')))
+#         exit(1)
 
-    print("Employee {}'s todo list exported to {}"
-          .format(user.get('name'), file_name))
+#     # Export todo list to CSV
+#     file_name = "{}.csv".format(employee_id)
+#     with open(file_name, 'w', newline='') as csvfile:
+#         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+#         for task in todos:
+#             task_status = "True" if task["completed"] else "False"
+#             csv_writer.writerow([
+#                 employee_id,
+#                 user.get("name"),
+#                 task_status,
+#                 task["title"]
+#             ])
+
+#     print("Employee {}'s todo list exported to {}"
+#           .format(user.get('name'), file_name))
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,46 +10,56 @@ import sys
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) != 2:
-    #     print("Usage: ./2-export_to_JSON.py <employee_id>")
-    #     exit(1)
-
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # Get employee name
-    response = requests.get(url + "users/{}".format(employee_id))
-    if response.status_code == 200:
-        user = response.json()
-    else:
-        print("Failed to get employee name for ID {}".format(employee_id))
-        # exit(1)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
 
-    # Get employee's todo list
-    todos_response = requests.get(
-        url + "todos", params={"userId": employee_id})
-    if todos_response.status_code == 200:
-        todos = todos_response.json()
-    else:
-        print("Failed to get todo list for employee {}"
-              .format(user.get('name')))
-        # exit(1)
+# if __name__ == "__main__":
+#     employee_id = sys.argv[1]
+#     url = "https://jsonplaceholder.typicode.com/"
 
-    # Export todo list to JSON
-    file_name = "{}.json".format(employee_id)
-    todo_list = {
-        employee_id: [
-            {
-                "task": todo["title"],
-                "completed": todo["completed"],
-                "username": user.get("name")
-            }
-            for todo in todos
-        ]
-    }
+#     # Get employee name
+#     response = requests.get(url + "users/{}".format(employee_id))
+#     if response.status_code == 200:
+#         user = response.json()
+#     else:
+#         print("Failed to get employee name for ID {}".format(employee_id))
+#         # exit(1)
 
-    with open(file_name, 'w') as jsonfile:
-        json.dump(todo_list, jsonfile)
+#     # Get employee's todo list
+#     todos_response = requests.get(
+#         url + "todos", params={"userId": employee_id})
+#     if todos_response.status_code == 200:
+#         todos = todos_response.json()
+#     else:
+#         print("Failed to get todo list for employee {}"
+#               .format(user.get('name')))
+#         # exit(1)
 
-    print("Employee {}'s todo list exported to {}"
-          .format(user.get('name'), file_name))
+#     # Export todo list to JSON
+#     file_name = "{}.json".format(employee_id)
+#     todo_list = {
+#         employee_id: [
+#             {
+#                 "task": todo["title"],
+#                 "completed": todo["completed"],
+#                 "username": user.get("name")
+#             }
+#             for todo in todos
+#         ]
+#     }
+
+#     with open(file_name, 'w') as jsonfile:
+#         json.dump(todo_list, jsonfile)
+
+#     print("Employee {}'s todo list exported to {}"
+#           .format(user.get('name'), file_name))
