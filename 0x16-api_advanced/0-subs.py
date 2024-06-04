b7c718e8391e33ce3661c97abb9a33e8126e720a
gboigwe <geakande@gmail.com>
2024-06-04 14:46:30 +0100

    Function that queries the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,43 @@
 #!/usr/bin/python3
-"""
-A function that queries the Reddit API
-"""
-
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """API subreddit"""
+   """
+   Returns the number of subscribers for a given subreddit.
+   If the subreddit is invalid, returns 0.
+   """
+   url = f"https://www.reddit.com/r/{subreddit}/about.json"
+   headers = {"User-Agent": "python-requests/2.28.1"}
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "python-requests/2.28.1"}
+   try:
+       response = requests.get(url, headers=headers, allow_redirects=False)
+       response.raise_for_status()
+       data = response.json()
+       subscribers = data["data"]["subscribers"]
+       return subscribers
+   except (requests.exceptions.RequestException, KeyError):
+       return 0
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    except requests.RequestException as err:
-        return 0
+# #!/usr/bin/python3
+# """
+# A function that queries the Reddit API
+# """
+
+
+# import requests
+
+
+# def number_of_subscribers(subreddit):
+#     """API subreddit"""
+
+#     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+#     headers = {"User-Agent": "python-requests/2.28.1"}
+
+#     try:
+#         response = requests.get(url, headers=headers, allow_redirects=False)
+#         response.raise_for_status()
+#         data = response.json()
+#         subscribers = data["data"]["subscribers"]
+#         return subscribers
+#     except requests.RequestException as err:
+#         return 0
