63ee565ab4593d073353dd24efd763af5f87758b
gboigwe <geakande@gmail.com>
2024-06-04 16:13:08 +0100

    Function that queries the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,30 +19,3 @@ def number_of_subscribers(subreddit):
         return (r.json()["data"]["subscribers"])
     else:
         return (0)
-
-# #!/usr/bin/python3
-# """API subreddit checking"""
-
-
-# import json
-# import requests
-# import sys
-
-
-# def number_of_subscribers(subreddit):
-#     """
-#     Returns the number of subscribers for a given subreddit.
-#     If the subreddit is invalid, returns 0.
-#     """
-
-#     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-#     headers = {"User-Agent": "python-requests/2.28.1"}
-
-#     try:
-#         response = requests.get(url, headers=headers, allow_redirects=False)
-#         response.raise_for_status()
-#         data = response.json()
-#         subscribers = data["data"]["subscribers"]
-#         return subscribers
-#     except (requests.exceptions.RequestException, KeyError):
-#         return 0
