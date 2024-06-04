f5d186d1c969f80f2fd9a6ae89082fc310e7319e
gboigwe <geakande@gmail.com>
2024-06-04 17:45:29 +0100

    Function that queries the Reddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+"""API subreddit checking"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the top 10 hot posts from a subreddit.
+    Args:
+        subreddit: The name of the subreddit (without 'r/').
+        Prints the titles or None if invalid subreddit.
+    """
+
+    url = "https://reddit.com/r/{}/hot.json?limit=10&raw_json=1".format(
+        subreddit)
+
+    try:
+        response = requests.get(url, headers={"User-Agent": "MyCoolScript"},
+                                allow_redirects=False)
+        response.raise_for_status()  # Raise exception for non-2xx status codes
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children'][:10]:
+                print(post['data']['title'])
+            else:
+                print(None)
+
+    except requests.exceptions.RequestException:
+        print(None)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+        print(None)
